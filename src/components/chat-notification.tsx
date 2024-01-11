dcac5ff6ca28f20c6350cbfa4984cfd3feb80c23
Bingchatbot <135514537+Bingchatbot@users.noreply.github.com>
2024-01-11 16:34:23 +0300

    Update chat-notification.tsx

--- src/components/chat-notification.tsx
+++ src/components/chat-notification.tsx
@@ -13,57 +13,57 @@ function getAction(error: ChatError, reset: () => void) {
     reset()
     return (
       <div>
-        请求次数过快，已被限流，请稍候重试...
+        Количество запросов слишком быстрое и ограничено. Повторите попытку позже...
       </div>
     )
   }
   if (error.code === ErrorCode.BING_IP_FORBIDDEN) {
     return (
       <ExternalLink href="https://github.com/weaigc/bingo/issues">
-        你的服务器或代理已被封禁，请更换服务器或使用代理重试
+        Ваш сервер или прокси-сервер заблокирован. Смените сервер или используйте прокси-сервер, чтобы повторить попытку.
       </ExternalLink>
     )
   }
   if (error.code === ErrorCode.BING_TRY_LATER) {
     return (
       <a href={`#dialog="reset"`}>
-        请求失败，请手动重试
+        Не удалось выполнить запрос. Повторите попытку вручную.
       </a>
     )
   }
   if (error.code === ErrorCode.BING_FORBIDDEN) {
     return (
       <ExternalLink href="https://bing.com/new">
-        你的账号已在黑名单，请尝试更换账号及申请解封
+        Ваш аккаунт занесен в черный список. Попробуйте сменить аккаунт и подать заявку на разблокировку.
       </ExternalLink>
     )
   }
   if (error.code === ErrorCode.CONVERSATION_LIMIT) {
     return (
       <div>
-        当前话题已中止，请点击
-        <a href={`#dialog="reset"`}>重新开始</a>
-        开启新的对话
+        Текущая тема приостановлена, пожалуйста, нажмите
+        <a href={`#dialog="reset"`}>Начать заново</a>
+        чтобы начать новый разговор
       </div>
     )
   }
   if (error.code === ErrorCode.BING_CAPTCHA) {
     return (
       <ExternalLink href="https://www.bing.com/turing/captcha/challenge">
-        点击通过人机验证
+        Нажмите, чтобы пройти CAPTCHA
       </ExternalLink>
     )
   }
   if (error.code === ErrorCode.BING_UNAUTHORIZED) {
     reset()
     return (
-      <a href={`#dialog="settings"`}>没有获取到用户信息或用户信息失效，点此重新设置</a>
+      <a href={`#dialog="settings"`}>Информация пользователя не была получена или информация пользователя недействительна, нажмите здесь, чтобы сбросить</a>
     )
   }
   if (error.code === ErrorCode.BING_IMAGE_UNAUTHORIZED) {
     reset()
     return (
-      <a href={`#dialog="settings"`}>画图需要用户信息，系统没有获取到有效的用户信息，点此设置</a>
+      <a href={`#dialog="settings"`}>Для рисования требуется информация пользователя. Система не получила достоверную информацию о пользователе. Нажмите здесь, чтобы настроить</a>
     )
   }
   return error.message
